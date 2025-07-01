9ece4c7edfab075a4163cdcd28280da1abaf7150
Gombos Vidor Márton <gombosvidor@gmail.com>
2025-07-01 17:42:30 +0200

    Added team name validation

--- Client/src/Quiz.jsx
+++ Client/src/Quiz.jsx
@@ -30,36 +30,46 @@ const Quiz = () => {
         "endTime": "13:55:00"
     }
 
-    const { t } = useLanguage();
+    const { t, teamName } = useLanguage();
+
+    const isQuizActive = false;
 
     const formattedTime = data.endTime.split(':').slice(0, 2).join(':');
 
     return (
         <>
             <div className="navbar bg-base-200 shadow-sm px-5 sticky top-0 z-50 justify-between">
-                <p className="text-lg font-bold truncate">Csapat neve</p>
-                <p className='text-lg'>{formattedTime}</p>
+                <p className="text-lg font-bold truncate">{teamName}</p>
+                <p className='text-lg'>{isQuizActive ? formattedTime : ''}</p>
             </div>
-            <div className='grid grid-cols-1 gap-2 p-3'>
-                {Object.entries(data.quizdata).map(([key, value]) => (
-                    <div key={key} className="card bg-base-100 shadow-xl">
-                        <div className="flex flex-row justify-between items-center p-5 card-body">
-                            <div>
-                                <h2 className="card-title">{value.name}</h2>
-                                {value.city === "-" ? (
-                                    <p>{value.country}</p>
-                                ) : (
-                                    <p>{value.city}, {value.country}</p>
-                                )}
+            {isQuizActive ? (
+                <div>
+                    <div className='grid grid-cols-1 gap-2 p-3'>
+                        {Object.entries(data.quizdata).map(([key, value]) => (
+                            <div key={key} className="card bg-base-100 shadow-xl">
+                                <div className="flex flex-row justify-between items-center p-5 card-body">
+                                    <div>
+                                        <h2 className="card-title">{value.name}</h2>
+                                        {value.city === "-" ? (
+                                            <p>{value.country}</p>
+                                        ) : (
+                                            <p>{value.city}, {value.country}</p>
+                                        )}
+                                    </div>
+                                    <input type="number" className="input validator input-sm w-12" required min={1} max={100} />
+                                </div>
                             </div>
-                            <input type="number" className="input validator input-sm w-12" required min={1} max={100} />
-                        </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className='flex justify-center p-5'>
-                <button className="btn btn-primary btn-wide">{t("finish")}</button>
-            </div>
+                    <div className='flex justify-center p-5'>
+                        <button className="btn btn-primary btn-wide">{t("finish")}</button>
+                    </div>
+                </div>
+            ) : (
+                <div className="flex justify-center items-center p-10">
+                    <p className="text-2xl font-bold">{t("quiz_not_started")}</p>
+                </div>
+            )}
         </>
     )
 }
--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 import { useLanguage } from './App'
 
 const Register = () => {
-    const { language, changeLng, t } = useLanguage();
+    const { language, changeLng, t, toQuiz, teamName, setTeamName } = useLanguage();
+
+    const [nameError, setNameError] = useState(false);
 
     const isLanguageSelected = (lng) => {
         return language === lng ? " btn-active" : "";
+    }
+
+    function handleInputChange(e) {
+        setTeamName(e.target.value);
+    }
+
+    function handleButtonClick() {
+        if (teamName.trim() === "") {
+            setNameError(true);
+            return;
+        }
+        toQuiz();
     }
 
     return (
@@ -17,9 +31,10 @@ const Register = () => {
             <div className="flex flex-col items-center content-center gap-5 w-2/3">
                 <fieldset className="fieldset w-full max-w-sm">
                     <legend className="fieldset-legend">{t("team_name")}</legend>
-                    <input type="text" className="input w-full" placeholder={t("type_here")} />
+                    <input type="text" className={"input w-full " + (nameError ? "input-error" : "")} placeholder={t("type_here")} value={teamName} onChange={(e) => handleInputChange(e)} />
+                    {nameError ? <p className="label text-error">{t("team_name_required")}</p> : null}
                 </fieldset>
-                <button className="btn btn-primary btn-wide">{t("continue")}</button>
+                <button className="btn btn-primary btn-wide" onClick={handleButtonClick}>{t("continue")}</button>
             </div>
         </div>
     )
