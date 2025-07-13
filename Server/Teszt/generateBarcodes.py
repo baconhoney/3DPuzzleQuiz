2201dc34ea87764e3933ff8330f2fd211ab365f8
baconhoney <hmartin9811@gmail.com>
2025-07-13 18:43:44 +0200

    API specs updated

--- Server/Teszt/generateBarcodes.py
+++ Server/Teszt/generateBarcodes.py
@@ -3,5 +3,4 @@ from PIL import Image
 
 img = io.BytesIO()
 barcode.generate('code128', '7648|Name', output=img, writer=barcode.writer.ImageWriter(), text="7648")
-img.seek(0)
 Image.open(img).show()
