3c68943c9a144aaf8571537c27006fbf67c05bbd
Hegedűs Kristóf <123968407+Kikka12@users.noreply.github.com>
2025-07-29 18:42:13 +0200

    removed base folder

--- SearchWebpage/vite.config.js
+++ SearchWebpage/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/search/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["gombosnet.ddns.net", "cicanet.ddns.net"],
