3d9e3916919239f861158f528df322c189b16a8c
DragonoidOmega <omegadragonoidomega@gmail.com>
2025-08-03 18:09:40 +0300

    implemented data extraction for printQuiz

---
+++ Server/printQuizTestThisHasToBeHere.py
@@ -0,0 +1,12 @@
+import dotenv
+import os
+import pathlib
+
+
+os.environ["CWD"] = str(pathlib.Path(__file__).parent.resolve().as_posix())
+dotenv.load_dotenv()
+import modules.printer as printer
+import modules.utils as utils
+
+
+printer.printQuiz(1, "hu", utils.QuizSize.SIZE_20)
