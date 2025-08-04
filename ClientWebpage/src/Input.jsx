38ed50902be8b09a807463043736db57a6d25411
Gombos Vidor Márton <gombosvidor@gmail.com>
2025-08-04 19:48:21 +0200

    Added custom input component

---
+++ ClientWebpage/src/Input.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Input = ({ id, type, className, min, max, defaultValue, onInput }) => {
+
+    const defaultClass = "focus-visible:border-gray-400 border-gray-500 focus:border-gray-500 focus-visible:ring-0 focus:outline-none";
+    const validateClass = "border-info focus:border-info focus-visible:ring-0 focus:outline-none";
+
+    const [extraClass, setExtraClass] = React.useState(() => {
+        return defaultValue ? validateClass : defaultClass;
+    });
+
+    function handleInputChange(e) {
+        if (e.target.value) {
+            setExtraClass(validateClass);
+        } else {
+            setExtraClass(defaultClass);
+        }
+    }
+
+    return (
+        <input
+            id={id}
+            type={type}
+            className={className + ' ' + extraClass}
+            min={min}
+            max={max}
+            defaultValue={defaultValue}
+            onInput={onInput}
+            onChange={handleInputChange}
+        />
+    )
+}
+
+export default Input
